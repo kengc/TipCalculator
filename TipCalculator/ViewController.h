a4946ecaf41f66cfb8631ea355c035fb906fc5f8
kevin Cleathero <kengc@hotmail.com>
2017-06-09 08:17:06 -0700

    project setup

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Kevin Cleathero on 2017-06-09.
+//  Copyright © 2017 Kevin Cleathero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TipCalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TipCalculator
+//
+//  Created by Kevin Cleathero on 2017-06-09.
+//  Copyright © 2017 Kevin Cleathero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
